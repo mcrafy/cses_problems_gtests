dec2e9391c34820046725151597cc8232b4c00ba
mcrafy <raphael.bieniek@stud.hn.de>
2026-04-04 00:23:07 +0200

    added solution for apple_division.cpp

--- src/01_Introductory/apple_division.cpp
+++ src/01_Introductory/apple_division.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <cstdlib>
 
-long long solve_apple_division(int n, const std::vector<int> &P)
+long long solve_apple_division(int n, std::vector<long long> &P)
 {
-    long long min;
-    return min;
+    long long total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total += P[i];
+    }
+
+    long long min_diff = LLONG_MAX;
+    for (int mask = 0; mask < 1 << n; ++mask)
+    {
+        long long subset = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            if (mask & 1 << j)
+            {
+                subset += P[j];
+            }
+            long long diff = abs(total - 2 * subset);
+
+            if (diff < min_diff)
+            {
+                min_diff = diff;
+            }
+        }
+
+    }
+    return min_diff;
 }
 
 #ifndef RUNNING_TESTS
--- tests/01_Introductory/apple_division_test.cpp
+++ tests/01_Introductory/apple_division_test.cpp
@@ -1,22 +1,25 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-long long solve_apple_division(int n, const std::vector<long long> &P);
+long long solve_apple_division(int n,  std::vector<long long> &P);
 
 TEST(Introductory, AppleDivision_BaseCase)
 {
+    std::vector<long long> test = {3, 2, 7, 4, 1};
     int expected = 1;
-    EXPECT_EQ(solve_apple_division(5, std::vector<long long>{3, 2, 7, 4, 1}), expected);
+    EXPECT_EQ(solve_apple_division(5,  test), expected);
 }
 
 TEST(Introductory, AppleDivision_EdgeCaseBig)
 {
+    std::vector<long long> test = {3, 2, 7, 100};
     int expected = 88;
-    EXPECT_EQ(solve_apple_division(5, std::vector<long long>{3, 2, 7, 100}), expected);
+    EXPECT_EQ(solve_apple_division(4, test), expected);
 }
 
 TEST(Introductory, AppleDivision_EdgeCaseOne)
 {
+    std::vector<long long> test = {5};
     int expected = 5;
-    EXPECT_EQ(solve_apple_division(1, std::vector<long long>{5}), expected);
+    EXPECT_EQ(solve_apple_division(1, test), expected);
 }
