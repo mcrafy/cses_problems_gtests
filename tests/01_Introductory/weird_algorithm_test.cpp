51216859b9ada75cdd2d887711aeb08f433dfb23
mcrafy <raphael.bieniek@stud.hn.de>
2026-04-03 21:06:08 +0200

    added test for weird_algorithm.cpp

--- tests/01_Introductory/weird_algorithm_test.cpp
+++ tests/01_Introductory/weird_algorithm_test.cpp
@@ -7,9 +7,11 @@
 std::vector<long long> solve_weird_algorithm(long long n);
 
 TEST(Introductory, WeirdAlgorithm_BaseCase) {
-
+    std::vector<long long> expected = {3, 10, 5, 16,8, 4, 2, 1};
+    EXPECT_EQ(solve_weird_algorithm(3), expected);
 }
 
 TEST(Introductory, WeirdAlgorithm_EdgeCaseOne) {
-
+    std::vector<long long> expected = {1};
+    EXPECT_EQ( solve_weird_algorithm(1), expected);
 }
